cf5b44f565112f11d5e4aacbd9da08827b40a08e
Daryll Wong <daryll_wong@hotmail.com>
2019-12-28 05:53:17 +0800

    combine both button and potentiometer

--- ArduinoTesting/BasicButtonANDPotentiometerLED/BasicButtonANDPotentiometerLED.ino
+++ ArduinoTesting/BasicButtonANDPotentiometerLED/BasicButtonANDPotentiometerLED.ino
@@ -1,44 +1,74 @@
-/******************************************/
+// There are 2 simple setups
+// 1) Push-button (i/p: 2) to light up LED (o/p: 13)
+// 2) Potentiometer (i/p: A0) to light up LED (o/p: 9)
+//_______________________________________________________________________________
 
-const int analogPin = 0;//the analog input pin attach to
+// For Button-LED
+const int buttonPin = 2;//the push-button pin
+const int ledButtonPin = 13;
+int inputButtonVal = 0; //variable to store the value from button
+int correctButtonVal = 0;//variable to store the CORRECT value from button - 0 means not pressed, 1 means pressed
 
-const int ledPin = 9;//the led attach to
 
-int inputValue = 0;//variable to store the value coming from sensor
+// For Potentiometer-LED
+const int analogPotentiometerPin = 0;//the analog input pin of potentiometer
+const int ledPotPin = 9;
+int inputPotVal = 0;//variable to store the analog value coming from potentiometer
+int outputLedPotVal = 0;//variable to store the output value (after being mapped)
 
-int outputValue = 0;//variable to store the output value
-
-/******************************************/
+//_______________________________________________________________________________
 
 void setup()
 {
 
   Serial.begin(9600);//set the serial communication baudrate as 9600
 
+  // For Button-LED
+  //configure buttonPin(pin2) as an input and enable the internal pull-up resistor
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledButtonPin, OUTPUT);
+
 }
 
-/******************************************/
+//_______________________________________________________________________________
 
 void loop()
 {
+  //__________For Button-LED__________
+  inputButtonVal = digitalRead(buttonPin);
+  correctButtonVal = !inputButtonVal; // button pressed = 1, not pressed = 0 
+  Serial.print("Button: ");
+  Serial.println(correctButtonVal);
+  // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
+  // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
+  // button's pressed, and off when it's not:
+  if (inputButtonVal == HIGH) {
+    digitalWrite(ledButtonPin, LOW);
+  } else {
+    digitalWrite(ledButtonPin, HIGH);
+    Serial.println("Button pressed");
+  }
 
-  inputValue = analogRead(analogPin);//read the value from the potentiometer
 
-  Serial.print("Input: "); //print "Input"
+  //__________For Potentiometer-LED__________
+  inputPotVal = analogRead(analogPotentiometerPin);//read the value from the potentiometer
+  Serial.print("Input from Potentiometer: ");
+  Serial.println(inputPotVal);
+  
+  //outputLedPotVal = map(inputPotVal, 0, 1023, 0, 255); // Convert from 0-1023 proportional to the number of a number of from 0 to 255
+  outputLedPotVal = map(inputPotVal, 0, 658, 0, 255); // Calibrated to 658 - tested max analog reading of potentiometer
 
-  Serial.println(inputValue); //print inputValue
+  Serial.print("Output to LED: ");
+  Serial.println(outputLedPotVal);
+  
+  analogWrite(ledPotPin, outputLedPotVal); //tune the brightness of LED according to the potentiometer value
 
-  // outputValue = map(inputValue, 0, 1023, 0, 255); // Convert from 0-1023 proportional to the number of a number of from 0 to 255
-  outputValue = map(inputValue, 0, 658, 0, 255); // Calibrated to 658
 
-  Serial.print("Output: "); //print "Output"
 
-  Serial.println(outputValue); //print outputValue
+  Serial.println("_________________________"); //print "Output"
 
-  analogWrite(ledPin, outputValue); //turn the LED on depending on the output value
 
-  delay(1000);
-
+  //delay(1000); 
 }
 
-/*******************************************/
+//_______________________________________________________________________________
