351a91565821c37eb54ca0d4673c978b010caedf
Daryll Wong <daryll_wong@hotmail.com>
2020-01-11 13:33:59 +0800

    temp change

--- ArduinoTesting/SerialCommunicationWithMultipleVals_To_Max/SerialCommunicationWithMultipleVals_To_Max.ino
+++ ArduinoTesting/SerialCommunicationWithMultipleVals_To_Max/SerialCommunicationWithMultipleVals_To_Max.ino
@@ -18,15 +18,23 @@ void loop()
     serialvalue = Serial.read(); // read a byte of serial data
     started = 1; // set the started flag to on
   }
+  started = 1;
+  serialvalue = 10;
 
   if(started) // loop once serial data has been received
   {
     randomvalue = random(1000); // pick a new random number
     Serial.print(countervalue); // print the counter
+
+    
     Serial.print(" "); // print a space
     Serial.print(randomvalue); // print the random value
+
+    
     Serial.print(" "); // print a space
     Serial.print(serialvalue); // echo the received serial value
+
+    
     Serial.println(); // print a line-feed
     countervalue = (countervalue+1)%1000; // increment the counter
     delay(100); // pause
