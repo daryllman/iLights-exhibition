32c4ffd28fde2284670d3dbac1b237bffb1ac414
Daryll Wong <daryll_wong@hotmail.com>
2020-01-11 13:56:24 +0800

    simple ldr working checker to pin A1

--- ArduinoTesting/LDRval/LDRval.ino
+++ ArduinoTesting/LDRval/LDRval.ino
@@ -1,9 +1,18 @@
+
+const int ldrPin = A1;
+
 void setup() {
-  // put your setup code here, to run once:
+
+Serial.begin(9600);
+
+pinMode(ldrPin, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+int ldrVal = analogRead(ldrPin);
+Serial.println(ldrVal);
+
 
 }
